d55e33f75d57e3671ffbb55c2e53270ce49b6dde
athosportes <athosuti@gmail.com>
2022-04-11 08:10:15 -0300

    all

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
@@ -10,10 +11,14 @@ export function Home() {
     <Container>
       <Header>
         <HeaderContent>
-          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <Logo 
+            width={RFValue(108)} 
+            height={RFValue(12)} 
+          />
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car />
     </Container>
   );
 }
